94659a1db9f5a7052e0a9e75f1a47164b13c6a49
krzysiekdabrowski <65534531+krzysiekdabrowski@users.noreply.github.com>
2024-05-18 11:21:26 +0200

    Update task4_1.r

--- src/R/task4_1.r
+++ src/R/task4_1.r
@@ -1,17 +1,27 @@
+install.packages("caret")
+library(caret)
+
 heartdata <- read.csv("/Users/k.d.dabrowski/Downloads/heartdata.csv", header = TRUE, sep = "\t")
 
 cor.test(heartdata$biking, heartdata$smoking)
-
-# Check for normality of residuals with histogram and Q-Q plot
 hist(residuals(model), main = "Histogram of Residuals", xlab = "Residuals")
 qqnorm(residuals(model))
 qqline(residuals(model), col = "red")
-
-# Check for homoscedasticity with residuals vs. fitted values plot
 plot(fitted(model), residuals(model), main = "Residuals vs Fitted", xlab = "Fitted Values", ylab = "Residuals")
 abline(h = 0, col = "red")
 
 
-
 model <- lm(heartdisease ~ biking + smoking, data = heartdata)
+
 summary(model)
+par(mfrow = c(2, 2))  # Arrange plots in a 2x2 grid
+plot(model)
+
+train_control <- trainControl(method = "cv", number = 10)  # 10-fold cross-validation
+
+model <- train(heartdisease ~ biking + smoking, 
+               data = heartdata, 
+               method = "lm", 
+               trControl = train_control)
+print(model)
+
